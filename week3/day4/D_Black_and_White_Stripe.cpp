6604fc820a52917e2268b75832fdc2921e2d0864
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-11-14 23:07:04 +0600

    day 4

---
+++ week3/day4/D_Black_and_White_Stripe.cpp
@@ -0,0 +1,43 @@
+/**
+ *    author:  ahammadabdullah
+ *    created: 2024-11-14 22:08:00
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
+#define inp(a)        \
+    for (auto &x : a) \
+    cin >> x
+#define out(a)              \
+    for (const auto &x : a) \
+        cout << x << ' ';   \
+    cout << '\n'
+#define pii pair<int, int>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+}
+int main()
+{
+    FAST_IO;
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
