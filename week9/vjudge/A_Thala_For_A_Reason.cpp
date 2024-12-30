7990ef774a9387e062b203481e816347265b481e
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2024-12-30 22:18:53 +0600

    vjduge

---
+++ week9/vjudge/A_Thala_For_A_Reason.cpp
@@ -0,0 +1,39 @@
+/**
+ *    author:  ahammadabdullah
+ *    created: 2024-12-30 21:19:22
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
+#define inp(a)        \
+    for (auto &x : a) \
+    cin >> x
+#define out(a)              \
+    for (const auto &x : a) \
+        cout << x << ' ';   \
+    cout << '\n'
+#define pii pair<int, int>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define MOD 1000000007
+int main()
+{
+    FAST_IO;
+    int n;
+    cin >> n;
+    if (n == 7)
+        cout << "THALA" << nl;
+    else
+        cout << "SADGE" << nl;
+    return 0;
+}
