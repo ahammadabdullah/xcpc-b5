0f31e08908ba19fce2397aff2b4d82079c0d8744
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-01-06 21:32:21 +0600

    vjudge

---
+++ week10/vjudge/A_Valentines_Contest.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  ahammadabdullah
+ *    created: 2025-01-06 21:05:46
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 121)
+        cout << "Likely" << endl;
+    else
+        cout << "Unlikely" << endl;
+    return 0;
+}
