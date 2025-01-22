57edcc17528333abcc98602480a93c60a4c7efef
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-01-22 21:54:11 +0600

    cc

---
+++ week12/cc/Access_Code_Equality.cpp
@@ -0,0 +1,41 @@
+/**
+ *    author:  ahammadabdullah
+ *    created: 2025-01-22 21:30:05
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
+#define inp(a)        \
+    for (auto &x : a) \
+    cin >> x
+#define out(a)              \
+    for (const auto &x : a) \
+        cout << x << ' ';   \
+    cout << '\n'
+#define pii pair<int, int>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define MOD 1000000007
+int main()
+{
+    FAST_IO;
+
+    string s;
+    cin >> s;
+    string code = "WECNITK";
+    if (s == code)
+        cout << "Welcome to Web Club!" << nl;
+    else
+        cout << "Access denied" << nl;
+    return 0;
+}
---
+++ week12/cc/Monster_Monster.cpp
@@ -0,0 +1,53 @@
+/**
+ *    author:  ahammadabdullah
+ *    created: 2025-01-22 21:35:57
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
+#define inp(a)        \
+    for (auto &x : a) \
+    cin >> x
+#define out(a)              \
+    for (const auto &x : a) \
+        cout << x << ' ';   \
+    cout << '\n'
+#define pii pair<int, int>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define MOD 1000000007
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> v(n);
+    inp(v);
+    sort(all(v));
+    ll cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ll hp = v[i] + (n - 1 - i) * x;
+        cnt = max(cnt, hp);
+    }
+    cout << cnt << nl;
+}
+int main()
+{
+    FAST_IO;
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
